53adf39f5e4cf32c527a745bf24cf8c84c2b5465
John Evans <john@milo.com>
2010-03-14 22:58:31 -0700

    Added initial install script.

---
+++ bin/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -d ~/dotfiles ]; then
+   echo "~/dotfiles does not exist.  aborting."
+   exit
+fi
+
+RM=/bin/rm
+LN=/bin/ln
+
+$(RM) ~/.bashrc
+$(RM) ~/.profile
+
+$(LN) -s ~/.bashrc ~/dotfiles/bashrc
+$(LN) -s ~/.profile ~/dotfiles/profile

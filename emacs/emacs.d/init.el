790b0716fdbb520ae346dff37972a73bb093ec19
John Evans <lgastako@gmail.com>
2013-11-18 15:24:20 -0800

    Add kibit and paredit

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -34,6 +34,29 @@
 (yas/load-directory yas/root-directory)
 (yas-global-mode 1)
 
+
+;; Teach compile the syntax of the kibit output
+(require 'compile)
+(add-to-list 'compilation-error-regexp-alist-alist
+         '(kibit "At \\([^:]+\\):\\([[:digit:]]+\\):" 1 2 nil 0))
+(add-to-list 'compilation-error-regexp-alist 'kibit)
+
+;; A convenient command to run "lein kibit" in the project to which
+;; the current emacs buffer belongs to.
+(defun kibit ()
+  "Run kibit on the current project.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile "lein kibit"))
+
+(defun kibit-current-file ()
+  "Run kibit on the current file.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile (concat "lein kibit " buffer-file-name)))
+
+
+
 (add-hook 'python-mode-hook 'fci-mode)
 
 ;; Pretty Font

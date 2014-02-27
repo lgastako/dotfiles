123767d77fb27ab886c416dbcaef2db8b8c25c4c
John Evans <lgastako@gmail.com>
2014-02-27 11:08:36 -0800

    lg and eastwood and slamhound.

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,4 +1,8 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-midje "3.0.0"]
                   [lein-exec "0.3.1"]
-                  [gluon "0.1.0-SNAPSHOT"]]}}
+                  [jonase/eastwood "0.1.0"]
+                  [slamhound "1.5.1"]
+;;                  [gluon "0.1.0-SNAPSHOT"]
+                 ]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

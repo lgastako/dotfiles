d1edc52ac1479f45942e20466da27a2d7b2d0538
John Evans <lgastako@gmail.com>
2014-02-28 01:45:21 -0800

    zoom zoom

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-midje "3.0.0"]
-                  [lein-exec "0.3.1"]]}}
+                  [lein-exec "0.3.1"]
+                  [com.jakemccrary/lein-test-refresh "0.3.4"]]}}

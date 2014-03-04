888a14c209ba58120447a0892310091495014512
John Evans <lgastako@gmail.com>
2014-03-03 22:53:28 -0800

    uh oh

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,4 +1,12 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-midje "3.0.0"]
                   [lein-exec "0.3.1"]
-                  [gluon "0.1.0-SNAPSHOT"]]}}
+                  [lein-ancient "0.5.4" :exclusions [org.clojure/clojure
+                                                     org.clojure/tools.reader
+                                                     commons-codec]]
+                  [gluon "0.1.0-SNAPSHOT"]]
+        :dependencies [[slamhound "1.5.1"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+
+
+

75bf4047a510d95cbde1f4d3463f054f69c2ff34
John Evans <lgastako@gmail.com>
2014-03-05 20:31:27 -0800

    chkpt

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,12 +1,23 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
+                  [lein-ritz "0.7.0"]
                   [lein-midje "3.0.0"]
                   [lein-exec "0.3.1"]
+                  ;; [xwatch "0.1.0-SNAPSHOT"]
+                  [lein-simpleton "1.2.0"]
+                  [lein-pprint "1.1.1"]
                   [lein-ancient "0.5.4" :exclusions [org.clojure/clojure
                                                      org.clojure/tools.reader
                                                      commons-codec]]
                   [jonase/eastwood "0.1.0"]
-;;                  [gluon "0.1.0-SNAPSHOT"]
-;;                  [com.jakemccrary/lein-test-refresh "0.3.4"]
+                  [hiccup-bridge "1.0.0-SNAPSHOT"]
+                  ;; [gluon "0.1.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.3.4"]
                   [slamhound "1.5.1"]]
-        :dependencies [[slamhound "1.5.1"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :dependencies [[slamhound "1.5.1"]
+                       [ritz/ritz-nrepl-middleware "0.7.0"]
+                       [nrepl-inspect "0.3.0"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :repl-options {:nrepl-middleware [ritz.nrepl.middleware.javadoc/wrap-javadoc
+                                          ritz.nrepl.middleware.simple-complete/wrap-simple-complete
+                                          ritz.nrepl.middleware.apropos/wrap-apropos
+                                          inspector.middleware/wrap-inspect]}}}

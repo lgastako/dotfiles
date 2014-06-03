1271902a0f826058ae9c1f63f37f5bad8ae35edd
John Evans <lgastako@gmail.com>
2014-06-02 17:07:08 -0700

    Things.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -205,6 +205,7 @@ Display the results in a hyperlinked *compilation* buffer."
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+(add-hook 'go-mode-hook               #'enable-paredit-mode)
 
 (autoload 'markdown-mode "markdown-mode.el"	"Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))

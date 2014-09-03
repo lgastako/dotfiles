21add7303f551a31a151fd551258834d8824111f
John Evans <lgastako@gmail.com>
2014-09-02 17:16:51 -0700

    This stuff.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -284,7 +284,7 @@ Display the results in a hyperlinked *compilation* buffer."
 ; Load el4r, which loads Xiki
 (add-to-list 'load-path "/Library/Ruby/Gems/2.0.0/gems/trogdoro-el4r-1.0.10/data/emacs/site-lisp/")
 ;;(add-to-list 'load-path "~/.rvm/gems/ruby-1.9.3-p448/gems/trogdoro-el4r-1.0.10/data/emacs/site-lisp/")
-(require 'el4r)
+;; (require 'el4r)
 ;; (el4r-boot)
 ;; (el4r-troubleshooting-keys)
 

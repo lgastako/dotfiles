caceb7a1cc47aeb4dfc18f24d00d4aedb780d9f8
John Evans <lgastako@gmail.com>
2014-12-01 14:40:12 -0500

    More pythonicism.

---
+++ emacs/emacs.d/elpa/concurrent-20140609.1940/concurrent-autoloads.el
@@ -0,0 +1,15 @@
+;;; concurrent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("concurrent.el") (21628 50088 203869 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; concurrent-autoloads.el ends here

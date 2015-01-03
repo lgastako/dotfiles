532adc2e9169d2a826f0151fea399a976d5dd5e7
John Evans <lgastako@gmail.com>
2015-01-03 18:09:46 -0500

    This.

--- emacs/emacs.d/languages/haskell.el
+++ emacs/emacs.d/languages/haskell.el
@@ -3,3 +3,6 @@
 ;; We need to establish both an interaction mode and an indentation mode.
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;; Now, how do we make sandboxes work...
+(setq haskell-process-type 'cabal-repl)

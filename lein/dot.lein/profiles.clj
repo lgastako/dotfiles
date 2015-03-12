791f8cbeca17ae295977f44984d09209b62c41e4
John Evans <lgastako@gmail.com>
2015-03-12 13:36:04 -0700

    Update ancient to avoid node rewrite error.

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,13 +1,13 @@
 {:user {:env {:env :dev ;; so you can check "(= (env :env) :dev)
               }
-        :plugins [;;[lein-kibit "0.0.8"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  ;; [lein-bin "0.3.4"]
-                  ;; [lein-exec "0.3.1"]
-                  ;; [lein-simpleton "1.2.0"]
-                  ;; [lein-ancient "0.5.4" :exclusions [org.clojure/clojure
-                  ;;                                    org.clojure/tools.reader
-                  ;;                                    commons-codec]]
-                  ;; [jonase/eastwood "0.1.0"]
-                  ;; [lein-try "0.4.1"]
+        :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.5" :exclusions [org.clojure/clojure
+                                                     org.clojure/tools.reader
+                                                     commons-codec]]
+                  #_ [lein-bin "0.3.4"]
+                  #_ [lein-exec "0.3.1"]
+                  #_ [lein-kibit "0.0.8"]
+                  #_ [lein-simpleton "1.2.0"]
+                  #_ [lein-try "0.4.1"]
+                  #_ [jonase/eastwood "0.1.0"]
                   ]}}

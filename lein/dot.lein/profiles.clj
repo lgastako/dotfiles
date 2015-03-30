d5a420d3a24a215008384c73449f075b4fdde3b8
John Evans <lgastako@gmail.com>
2015-03-30 17:05:23 -0400

    These things.

--- lein/dot.lein/profiles.clj
+++ lein/dot.lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-ancient "0.6.5" :exclusions [org.clojure/clojure
                                                      org.clojure/tools.reader
-                                                     commons-codec]]]}}
+                                                     commons-codec]]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.2.1"]]}}

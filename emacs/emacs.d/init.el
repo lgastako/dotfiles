718a64042c2eab8e94a560e48f5c84966b51f8a4
John Evans <lgastako@gmail.com>
2016-03-11 17:47:35 -0500

    Add window rotation.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -760,6 +760,33 @@
 (put 'upcase-region   'disabled nil)
 (put 'downcase-region 'disabled nil)
 
+;; Allow rotating out-of-order windows.
+(defun rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the
+other direction.  Additionally given a numeric prefix argument n,
+it will rotate the windows n times; if the numeric argument is
+negative rotates |n| times in the other direction."
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (prefix-numeric-value arg))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse 'identity)))
+      (dotimes (_ (abs rotate-times))
+        (dotimes (i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)))))))
+
+(global-set-key (kbd "C-x r w") 'rotate-windows)
+
 ;; Experimental
 (global-set-key (kbd "C-c r a t") 'mc/mark-all-like-this-dwim)
 

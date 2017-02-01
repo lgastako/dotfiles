2e06b02bd2e2add957d370257a56c6f10c610ca2
John Evans <lgastako@gmail.com>
2017-02-01 00:57:53 -0500

    These make stuff work sort of?

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1077,3 +1077,12 @@
 
 
 ;;  '(haskell-process-type 'stack-ghci))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)))))

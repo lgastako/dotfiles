c89564e418b6fb77a8093b54728d1d86ddbde4d4
John Evans <lgastako@gmail.com>
2017-02-01 00:56:55 -0500

    Add terraform-mode package.

---
+++ emacs/emacs.d/elpa/terraform-mode-20170101.456/terraform-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "terraform-mode" "20170101.456" "Major mode for terraform configuration file" '((emacs "24.3") (hcl-mode "0.3")) :url "https://github.com/syohex/emacs-terraform-mode")

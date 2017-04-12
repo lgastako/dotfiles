717792ffefdbd868a92c55968413361ca4b0492e
John Evans <lgastako@gmail.com>
2017-04-12 12:29:43 -0400

    Latest refreshed package contents.

---
+++ emacs/emacs.d/elpa/memoize-20130421.1234/memoize-autoloads.el
@@ -0,0 +1,15 @@
+;;; memoize-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("memoize.el") (22760 16692 133102 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; memoize-autoloads.el ends here

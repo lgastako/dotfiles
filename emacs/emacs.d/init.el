0b39e966b44be613b2f8a39239e4fceb7cf22be5
John Evans <lgastako@gmail.com>
2017-06-05 23:49:08 -0600

    Update with new issues from June 2017.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -67,7 +67,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 235 :width normal :foundry "apple" :family "Monaco")))))
+
+;; '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 235 :width normal :foundry "apple" :family "Andale Mono")))))
+
+
 
 (if window-system
     (load-theme 'deeper-blue)
@@ -1075,6 +1080,9 @@
 (global-set-key (kbd "C-x w r") 'rotate-windows)
 
 (global-set-key (kbd "C-c a r") 'align-regexp)
+
+(global-set-key (kbd "C-c r s") 'replace-string)
+
 
 ;; Experimental
 (global-set-key (kbd "C-c r a t") 'mc/mark-all-like-this-dwim)

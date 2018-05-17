7e8bb8a9858048ae0985643da3a661a62b5f730a
John Evans <lgastako@gmail.com>
2018-05-16 23:32:49 -0600

    Disable asking about killing all buffers.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1214,6 +1214,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-ask-also-kill-buffers nil)
  '(haskell-process-type (quote stack-ghci))
  '(org-agenda-files (quote ("~/Dropbox/org/")))
  '(package-selected-packages

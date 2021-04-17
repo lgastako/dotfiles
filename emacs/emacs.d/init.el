b2c6cc203ee26950697169b65474a8b2d5ada8cd
John Evans <lgastako@gmail.com>
2021-04-17 04:38:22 -0400

    Get rid of jira org junk

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -493,10 +493,11 @@
                                (haskell . t)
                                (python . t)
                                ))
-  ;; jira
-  (use-package org-jira
-    :config
-    (setq jiralib-url "https://interos.atlassian.net")))
+  ;; ;; jira
+  ;; (use-package org-jira
+  ;;   :config
+  ;;   (setq jiralib-url "https://interos.atlassian.net"))
+  )
 
 (use-package magit
   :pin melpa-stable
@@ -1358,7 +1359,7 @@ vi style of % jumping to matching brace."
     ("~/SecuriSync/org/interos.org" "~/SecuriSync/org/roadmap/user-stories.org")))
  '(package-selected-packages
    (quote
-    (php-mode erc-sasl jinja2-mode graphql-mode protobuf-mode nix-buffer nix-shell nix-drv-mode nix-repl tide tj3-mode ess htmlize org-jira camcorder applescript-mode ein intero dumb-jump nix-mode dante cmake-mode csv-mode zencoding-mode yasnippet yaml-mode ws-trim which-key virtualenvwrapper utop use-package tuareg toml-mode terraform-mode swift-mode sly shakespeare-mode scala-mode2 sass-mode rust-mode revive restclient rainbow-mode rainbow-delimiters racket-mode quack pydoc-info psci psc-ide projectile paredit mwim multiple-cursors merlin memoize markdown-mode json-mode js2-mode ipython hydra hungry-delete helm-idris helm-git-grep helm-ag golden-ratio go-eldoc ghc geiser free-keys frame-cmds flymake-go flymake-cursor fill-column-indicator expand-region es-mode erlang elm-mode edn edit-server drag-stuff dockerfile-mode cython-mode csharp-mode coffee-mode cider beacon alchemist ace-window ace-jump-mode ac-helm)))
+    (php-mode erc-sasl jinja2-mode graphql-mode protobuf-mode nix-buffer nix-shell nix-drv-mode nix-repl tide tj3-mode ess htmlize camcorder applescript-mode ein intero dumb-jump nix-mode dante cmake-mode csv-mode zencoding-mode yasnippet yaml-mode ws-trim which-key virtualenvwrapper utop use-package tuareg toml-mode terraform-mode swift-mode sly shakespeare-mode scala-mode2 sass-mode rust-mode revive restclient rainbow-mode rainbow-delimiters racket-mode quack pydoc-info psci psc-ide projectile paredit mwim multiple-cursors merlin memoize markdown-mode json-mode js2-mode ipython hydra hungry-delete helm-idris helm-git-grep helm-ag golden-ratio go-eldoc ghc geiser free-keys frame-cmds flymake-go flymake-cursor fill-column-indicator expand-region es-mode erlang elm-mode edn edit-server drag-stuff dockerfile-mode cython-mode csharp-mode coffee-mode cider beacon alchemist ace-window ace-jump-mode ac-helm)))
  '(safe-local-variable-values
    (quote
     ((haskell-process-use-ghci . t)

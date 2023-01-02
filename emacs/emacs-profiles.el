b092b0db481dfff7e7659bd7a087127607a162ef
John Evans <lgastako@gmail.com>
2023-01-02 11:26:18 -0500

    Latest on rhadamanthus.

--- emacs/emacs-profiles.el
+++ emacs/emacs-profiles.el
@@ -1,5 +1,5 @@
-(("default" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.simple")))
+(("simple" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.simple")))
  ("busted" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.old-and-busted")))
- ("spacemacs" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.spacemacs")))
+ ("default" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.spacemacs")))
  ("doom" . ((user-emacs-directory . "~/dotfiles/emacs/emacs.d.doom"))))
 
